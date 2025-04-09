af45d1e5ff02dcdb506268bc25aa37ab2e1c5cde
tmd2311 <dung.kayc@gmail.com>
2025-04-10 04:44:00 +0700

    Fake login

--- QUAN LI CHI TIEU/app/src/main/java/com/example/quanlichitieu/MainActivity.java
+++ QUAN LI CHI TIEU/app/src/main/java/com/example/quanlichitieu/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.quanlichitieu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,29 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.quanlichitieu.UI.HomeCaNhanActivity;
 
+public class MainActivity extends AppCompatActivity {
+    EditText txtEmail, txtPassword;
+    Button btnSignin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.login);
+
+        txtEmail = findViewById(R.id.txtEmail);
+        txtPassword = findViewById(R.id.txtPassword);
+        btnSignin = findViewById(R.id.btnSignin);
+
+        btnSignin.setOnClickListener(v->{
+            if(!loginSuccess()){
+                Intent intent = new Intent(MainActivity.this, HomeCaNhanActivity.class);
+                startActivity(intent);
+            }
         });
+    }
+    private boolean loginSuccess() {
+        return txtEmail.getText().toString().isEmpty() && txtPassword.getText().toString().isEmpty();
     }
 }
